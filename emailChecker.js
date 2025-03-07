f5c75c080c65554237c87236055e07f8b7182977
Anshi Patel <anshipatel881@gmail.com>
2025-03-07 12:19:41 +0530

    [Anshi] Add . Added regex to validate email and check optional part xyz

--- emailChecker.js
+++ emailChecker.js
@@ -1,5 +1,5 @@
 function validateEmail(email){
-    let emailRegex = /^[a-zA-Z0-9]+(\.[a-zA-Z0-9]+)?@bridgelabz\.co(\.[a-zA-Z]{2,})?$/;
+    let emailRegex = /^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)?@bridgelabz\.co(\.[a-zA-Z]{2,})?$/;
     if(emailRegex.test(email)){
       console.log(email+ " is Valid Email ✅ ");
     }else{
@@ -12,7 +12,7 @@ function validateEmail(email){
   validateEmail("abc@bridgela.com");
   
   validateEmail("abc@.co.in");
-  validateEmail("abc.xyz@bridgelabz.tut");
+  validateEmail("abc.xyz@bridgelabz.co.gov");
   validateEmail("abc@bridgelabz.c");
   validateEmail("abc..xyz@bridgelabz.cin");
   validateEmail("abc@bridgelabz..co.in");
