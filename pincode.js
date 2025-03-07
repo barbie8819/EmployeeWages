851d35f6e590d3d82e5d5f6a8a3b7c1bb43e6bfb
Anshi Patel <anshipatel881@gmail.com>
2025-03-07 11:55:41 +0530

    [Anshi] Add . Added restrict special characters

--- pincode.js
+++ pincode.js
@@ -1,8 +1,16 @@
-const pinCode = Number(process.argv[2]);
-const pinCodeRegex = RegExp("[0-9]{6}")
-
-if(pinCodeRegex.test(pinCode)){
-  console.log("Pin code is valid !!!")
-}else{
-  console.log("Pin code is not valid !!!")
-}
+function validatePin(pin) {
+    let pinRegex = /^[0-9]{5}[0-9]$/;
+    if (pinRegex.test(pin)) {
+      console.log(pin + " is a valid PIN");
+    } else {
+      console.log(pin + " is an invalid PIN");
+    }
+  }
+  
+  validatePin("400088");
+  validatePin("123456");
+  
+  validatePin("400088B");
+  validatePin("400088#");
+  validatePin("40008A");
+  validatePin("1234");
